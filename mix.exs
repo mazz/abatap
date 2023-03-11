871644b2838131b9e4aca65c3f8b9ade61408cc1
Michael <mykelkim@yahoo.com>
2023-03-10 21:08:30 -0500

    preparation for publishing to hex.pm

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Abatap.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/mazz/abatap"
     ]
   end
 
@@ -23,6 +26,19 @@ defmodule Abatap.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "Generate letter avatar from username."
+  end
+
+  defp package() do
+    [
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["BSD-2-Clause"],
+      links: %{"GitHub" => "https://github.com/mazz/abatap"}
     ]
   end
 end
