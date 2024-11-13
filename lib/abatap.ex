5277eded1d3f4e89c2066fe7333c504d20670d54
Michael <mykelkim@yahoo.com>
2024-11-13 18:49:53 -0500

    test create_from_initials_image() to use Image

--- lib/abatap.ex
+++ lib/abatap.ex
@@ -15,6 +15,106 @@ defmodule Abatap do
     "rgb(#{r},#{g},#{b})"
   end
 
+  def create_from_initials_image(first_name, last_name, options \\ [])
+  when byte_size(first_name) > 0 and byte_size(last_name) > 0 do
+    resolution = 72
+    sampling_factor = 3
+
+    palette =
+      case Keyword.get(options, :palette) do
+        nil ->
+          :google
+
+        value ->
+          value
+      end
+
+    appearance =
+      case Keyword.get(options, :appearance) do
+        nil ->
+          :light
+
+        value ->
+          value
+      end
+
+    size =
+      case Keyword.get(options, :size) do
+        nil ->
+          512
+
+        value ->
+          value
+      end
+
+    font_size = Float.to_string(@font_size_ratio * size)
+
+    bg_color =
+      case palette do
+        :google ->
+          to_rgb(Abatap.Color.google_random())
+
+        :iwanthue ->
+          to_rgb(Abatap.Color.iwanthue_random())
+      end
+
+    fill_color =
+      case appearance do
+        :light ->
+          @fill_color_light
+
+        :dark ->
+          @fill_color_dark
+      end
+
+    initials = "#{String.at(first_name, 0)}#{String.at(last_name, 0)}"
+
+    image_path =
+      System.tmp_dir!()
+      |> Path.join(
+        "#{initials}-#{Atom.to_string(palette)}-#{Atom.to_string(appearance)}-#{:os.system_time(:milli_seconds)}.png"
+      )
+
+
+      {:ok, avatar} = Image.Text.text!(initials, background_fill_color: :cyan, font_size: 512, padding: 280) |> Image.avatar(shape: :square, size: 512)
+
+      Image.write!(avatar, image_path)
+    # System.cmd("convert", [
+    #   # sample up
+    #   "-density",
+    #   "#{resolution * sampling_factor}",
+    #   # corrected size
+    #   "-size",
+    #   "#{size * sampling_factor}x#{size * sampling_factor}",
+    #   # background color
+    #   "xc:#{bg_color}",
+    #   # text color
+    #   "-fill",
+    #   fill_color,
+    #   # @fill_color,
+    #   # font location
+    #   "-font",
+    #   "#{@font_filename}",
+    #   # font size
+    #   "-pointsize",
+    #   font_size,
+    #   # center text
+    #   "-gravity",
+    #   "center",
+    #   # render text in the center
+    #   "-annotate",
+    #   "+0+0",
+    #   "#{initials}",
+    #   # sample down to reduce aliasing
+    #   "-resample",
+    #   "#{resolution}",
+    #   image_path
+    # ])
+
+    {:ok, image_path}
+
+  end
+
   @doc """
   Generates an image using first name and last name of a user using imagemagick's convert command line tool.
 
